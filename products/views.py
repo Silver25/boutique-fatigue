f992ec3bdc89868cb9c0c2752e642fe24b31d396
Silver25 <silvio.martinec.ie@gmail.com>
2025-04-21 16:14:17 +0100

    update links for search form, create imports and request for search queries

--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+# If the query isn't blank, a special object from Jango.db.models 
+# called Q will generate a search query
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -11,11 +15,28 @@ def all_products(request):
 
     # return all products from the database using 'Product.objects.all'
     products = Product.objects.all()
+    query = None
+
+    # request for the search form
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+# In the case of queries when a user submits a query in order for it to match 
+# the term would have to appear in both the product name and the product description
+# we want to return results where the query was matched in either the product 
+# name or the description, to accomplish this 'OR' logic, we need to use 'Q'
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     # for products to be available in the template
     # to send some things back to the template
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
