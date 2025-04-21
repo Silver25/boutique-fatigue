607e890d24ab6e04c3b9118c0bd54bb1c7b5e285
Silver25 <silvio.martinec.ie@gmail.com>
2025-04-21 13:51:08 +0100

    setup a view for the product_detail.html page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -19,3 +19,21 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    docstring / description of the function:
+    A view to display details about one product
+    """
+
+    # return product from the database
+    product = get_object_or_404(Product, pk=product_id)
+
+    # for products to be available in the template
+    # to send some things back to the template
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
