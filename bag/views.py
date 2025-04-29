b1c22a073b5300a950bd972cef1310f41bc8071d
Silver25 <silvio.martinec.ie@gmail.com>
2025-04-29 20:01:11 +0100

    create new logic to implement 'size' variable

--- bag/views.py
+++ bag/views.py
@@ -14,15 +14,34 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    # to store/display information of the sizes of the product on shopping bag page
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     # variable to temporary store session information,
     # content to the bag and allow user to browse and
     # continue with shoping till browser is open
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # if statement to check if a product with sizes is being added
+    if size:
+        # If the item is already in the bag
+        if item_id in list(bag.keys()):
+            #  need to check if another item of the same id and same size already exists
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            # otherwise just set it equal to the quantity since the item already exists in the bag
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        # If the items not already in the bag we just need to add it
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    # otherwise if there's no size we run this next (original) logic
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     # to test form for adding product to bag 'print' the shopping bag
