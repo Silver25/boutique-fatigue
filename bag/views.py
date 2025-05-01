c4dfd013268d343f1556166f27301cb188844695
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-01 15:37:39 +0100

    Add new msgs in views

--- bag/views.py
+++ bag/views.py
@@ -36,19 +36,27 @@ def add_to_bag(request, item_id):
             #  need to check if another item of the same id and same size already exists
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
+                # message use 'product' to inform for products with sizes
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             # otherwise just set it equal to the quantity since the item already exists in the bag
             else:
                 bag[item_id]['items_by_size'][size] = quantity
+                # message use 'product' to inform for adding a new size for existed item in bag
+                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
         # If the items not already in the bag we just need to add it
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
+            # message use 'product' to inform for adding the item with a size
+            messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
     # otherwise if there's no size we run this next (original) logic
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            # message use 'product' to inform for updating the quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
-            # to demonstrate how these messages will work use 'product'
+            # message use 'product' to inform for adding items to the bag
             messages.success(request, f'Added {product.name} to your bag!')
 
     request.session['bag'] = bag
@@ -73,18 +81,27 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
+            # message use 'product' to inform for products with sizes
+            messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+                # message use 'product' to inform for adding a new size for existed item in bag
+                messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            # message use 'product' to inform for updating the quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
+            # message use 'product' to inform for removed from bag here when removing items
+            messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
 
 def remove_from_bag(request, item_id):
     """ Remove the item from the shopping bag """
