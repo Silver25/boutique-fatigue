8d8784e238e717df0f27e868a1268005d020e03c
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-03 20:00:40 +0100

    created file with code to update 'total'

---
+++ checkout/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+# receiver decorator to execute this function anytime the post_save signal is sent
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
+
+# receiver decorator to execute this function anytime the post_delete signal is sent
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
