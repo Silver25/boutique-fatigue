bf9fead173be0b93cc59a8028d70414fc409d993
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-07 17:34:34 +0100

    Set Stripe payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -18,6 +18,13 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
