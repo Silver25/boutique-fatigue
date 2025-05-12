fc3e369a8cbc39da430b02b20f9bb763cec86302
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-12 13:50:47 +0100

    update with new model field and import

--- checkout/models.py
+++ checkout/models.py
@@ -8,11 +8,13 @@ from django_countries.fields import CountryField
 
 # since the order line item model has a foreign key to it
 from products.models import Product
+from profiles.models import UserProfile
 
 
 # Postcode and County will not be required as those components don't exist in every Country
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
