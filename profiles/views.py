65152ff77665c49e28717baf6894c076c771fbaa
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-13 20:05:00 +0100

    add django decorators for security

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+# Decorators are special functions that wrap around another function
+# and return a new one with some additional functionality - security
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
 
 
+# Django first check whether the user is logged in
+# before executing the view and if not it'll redirect
+# them to the login page - securing the Admin only actions
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
