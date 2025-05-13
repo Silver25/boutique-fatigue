4e2c387c87349b3980c7a8a574459cd4c84637c3
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-13 13:16:45 +0100

    Add field and import

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -112,7 +113,9 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    
+                    # create orders for auth users by attaching their profile
+                    # or for non-auth users by setting that field to none
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
