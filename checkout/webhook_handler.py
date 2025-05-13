4ef72beb2ccb7f5ad1ba985cb9f0056fa5723629
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-13 13:12:06 +0100

    add checkout to handle user profiles if the checkout view fails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -57,6 +57,25 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None  # allow anonymous users to checkout
+        # if the username isn't anonymous user than is authenticated
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            # if the user is authenticated get their profile using their username
+            profile = UserProfile.objects.get(user__username=username)
+            # if the save info box is checked update their profile with default delivery information
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                # save the profile and all is set
+                profile.save()
+
         # if the order doesn't exist in the database
         order_exists = False
         # if view is slow and hasn't created the order by the time the webhook comes from stripe,
@@ -93,6 +112,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
