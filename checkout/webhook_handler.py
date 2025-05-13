8dbedcaf447d55be74e6c52e6621a6731a248e5b
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-13 14:34:43 +0100

    add calls to method for sending email

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -126,6 +126,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -173,7 +174,8 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        
+        # if the order was created by the webhook handler it'll send the email
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
