22dedf5041d59fee1c2a816271e7be35cbf134d6
Silver25 <silvio.martinec.ie@gmail.com>
2025-05-15 16:56:45 +0100

    add cache control

--- boutique/settings.py
+++ boutique/settings.py
@@ -205,6 +205,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # when the 'USE_AWS' [ Heroku Config vars ] setting is true whenever collectstatic
 # is run static files will be collected into a static folder in s3 bucket
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutiquefatique'
     AWS_S3_REGION_NAME = 'eu-north-1'
